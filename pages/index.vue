368654b734f922bd48cdffd3d4b3a2c1b6856be6
cb-sushank <sushank.sakthivel@chargebee.com>
2022-03-30 17:55:08 +0530

    v0.1

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-flex lg3 sm6 xs12>
+      <card 
+        icon="mdi-bookmark-check"
+        title="134"
+        sub-title="Matches"
+        color="green">
+      </card>
+    </v-flex>
+    <v-flex lg3 sm6 xs12>
+      <card 
+        icon="mdi-bookmark-remove"
+        title="7"
+        sub-title="Mismatches"
+        color="red">
+      </card>
+    </v-flex>
+  </v-row>
+</template>
+
+<script>
+import card from './card.vue'
+export default {
+  components: { card },
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/chargebee-icon-brand.png"
+        alt="Vuetify.js"
+        class="mb-5"
+        height="200px"
+        width="200px"
+      >
+      <blockquote class="blockquote">
+        Chargebee is the subscription billing and revenue 
+        management platform that lets you solve for your today, 
+        and scale for your tomorrow.
+        <footer>
+          <small>
+            <em>&mdash;Chargebee</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'InspirePage'
+}
+</script>
